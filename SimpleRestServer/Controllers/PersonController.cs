bfbfe20178cd8aa697460fd17c9188152635eb9c
Maurmair <maurmair@gmail.com>
2018-03-27 16:46:04 +0200

    First

---
+++ SimpleRestServer/Controllers/PersonController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SimpleRestServer.Models;
+namespace SimpleRestServer.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Person1", "Person2" };
+        }
+
+        // GET: api/Person/5
+        public Person Get(long id)
+        {
+            PersonPersistence pp = new PersonPersistence();
+            Person person = pp.getPerson(id);
+            /*person.ID = id;
+            person.LastName = "Smith";
+            person.FirstName = "John";
+            person.PayRate = "45.54";
+            person.StartDate = DateTime.Parse("5/5/1990");
+            person.EndDate = DateTime.Parse("1/1/2000");*/
+            return person;
+        }
+
+        // POST: api/Person        
+        public HttpResponseMessage Post([FromBody]Person value)
+        {
+            PersonPersistence pp = new PersonPersistence();
+            long id;
+            id = pp.savePerson(value);
+            value.ID = id;
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+            response.Headers.Location = new Uri(Request.RequestUri, String.Format("/{0}", id));
+            return response;
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SimpleRestServer/PersonPersistence.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using SimpleRestServer.Models;
+
+namespace SimpleRestServer
+{
+    public class PersonPersistence
+    {
+
+        private MySql.Data.MySqlClient.MySqlConnection conn;
+        public PersonPersistence()
+        {
+            string myConnectionString;
+            myConnectionString = "server=127.0.0.1;uid=root;pwd=RandomPw!;database=employeedb";
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+
+            }
+        }
+
+        public long savePerson(Person personToSave)
+        {
+            string sqlString = "INSERT INTO tblpersonnel (FirstName, LastName, PayRate, StartDate, EndDate) VALUES('" 
+                + personToSave.FirstName + "','" 
+                + personToSave.LastName + "'," 
+                + personToSave.PayRate + ",'" 
+                + personToSave.StartDate.ToString("yyyy-MM-dd HH:mm:ss") + "','" 
+                + personToSave.EndDate.ToString("yyyy-MM-dd HH:mm:ss") + "')".ToString(CultureInfo.CreateSpecificCulture("sv-SE"));
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            cmd.ExecuteNonQuery();
+            long id = cmd.LastInsertedId;
+            return id;
+            //.ToString(CultureInfo.CreateSpecificCulture("sv-SE"))
+        }
+
+        public Person getPerson(long ID)
+        {
+            Person p = new Person();
+            MySql.Data.MySqlClient.MySqlDataReader mySqlReader = null;
+            String sqlString = "SELECT * FROM tblpersonnel WHERE ID = " + ID.ToString();
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            mySqlReader = cmd.ExecuteReader();
+            if (mySqlReader.Read())
+            {
+                p.ID = mySqlReader.GetInt32(0);
+                p.FirstName = mySqlReader.GetString(1);
+                p.LastName = mySqlReader.GetString(2);
+                p.PayRate = mySqlReader.GetString(3);
+                p.StartDate = mySqlReader.GetDateTime(4);
+                p.EndDate = mySqlReader.GetDateTime(5);
+                return p;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
